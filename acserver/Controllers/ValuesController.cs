1f2efb620a14fa01e05b3df05923c6688b5f1826
vasko <vasko@mail.com>
2018-03-03 10:17:38 +0200

    Updated ac controller

--- acserver/Controllers/ValuesController.cs
+++ acserver/Controllers/ValuesController.cs
@@ -3,9 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using aclib;
 
 namespace acserver.Controllers
 {
+    [Route("api/ac")]
+    public class AcController
+    {
+        private AcManager _acManager;
+        private Ac _ac;
+
+        public AcController(AcManager acManager)
+        {
+            _acManager = acManager;
+            _ac = acManager.CreateAc("Daikin");
+        }
+
+        [HttpPost]
+        public string TurnOff(){
+            var cmd = new AcCommand().TurnOff();
+            _ac.Execute(cmd);
+            return "ok";
+        }
+        [HttpPost]
+        public string SetStatus(int temp, int fanPerc, string mode){
+            var cmd = new AcCommand()
+                .SetTemperature(temp)
+                .SetFanSpeed(_ac, (byte)fanPerc);
+            if(!string.IsNullOrEmpty(mode)){
+                if(mode=="cold") cmd.Cold();
+                if(mode=="heat") cmd.Heat();
+            }
+            _ac.Execute(cmd);
+            return "ok";
+        }
+    }
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
